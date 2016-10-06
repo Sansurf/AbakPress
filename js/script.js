75d095371f68f4e53d7cb244ea2d1306e204ed77
Eliseev Alexandr <sansurf@inbox.ru>
2016-10-07 01:46:36 +0500

    со списком городов пока закончил, принимаюсь за карусели

--- js/script.js
+++ js/script.js
@@ -1,31 +1,30 @@
 // Выпадающий список городов
 
-var list_cities = ["Екатеринбург", "Астрахань", "Воронеж", "Нижний Новгород", "Новосибирск"];
+var list_cities = ["Астрахань", "Воронеж", "Нижний Новгород",
+	"Новосибирск", "Краснодар", "Ростов", "Самара", "Уфа", "Казань",
+	"Пермь", "Красноярск", "Омск", "Волгоград", "Ханты-Мансийск",
+	"Москва", "Санкт-Петербург", "Екатеринбург", "Тюмень", "Челябинск"
+];
+
 var ul = document.getElementById('dropdown');
 
-for (i=0; i<list_cities.length; i++) {
-	
+for(i=0; i<list_cities.length; i++) {
 	var newLi = document.createElement('li');
-	newLi.setAttribute('id', 'li_city');
+	var newA = document.createElement('a');
+		newA.setAttribute('onclick', 'setCity("'+list_cities[i]+'")');
+		newA.href = '#';
+		newA.innerHTML = list_cities[i];
+	newLi.appendChild(newA);
 	ul.appendChild(newLi);
 }
 
-for (i=0; i<list_cities.length; i++) {
-
-	var newA = document.createElement('a');
-	newA.setAttribute('onclick', 'setCity()');
-	newA.href = '#';
-	newA.appendChild(document.createTextNode(list_cities[i]));
-	liElements = ul.getElementsByTagName('*');
-	liElements[i].appendChild(newA);
-}
-
-function setCity() {
-
+function setCity(city) {
+	ul.style.display = 'none';
+	var choosedCity = document.getElementById('choosed_city');
+	choosedCity.innerHTML = city;
 }
 
 function down() {
-	
 	if (ul.style.display == 'none')
 		ul.style.display = 'block';
 	else
